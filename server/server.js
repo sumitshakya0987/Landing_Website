a7bb8b8b873e105e557bdec609a1459a81b33836
sumitshakya0987 <shakyasumit@gmail.com>
2025-06-27 11:35:33 +0530

    Fix: Added Bootstrap dependency

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 connectDB();
 
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/auth', require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
